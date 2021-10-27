7d91a2eba40ef9b5eaf41ba2a1d8cb75bcaa322f
Ophir Yael <ofir.yael@gmail.com>
2021-10-27 22:46:15 +0300

    Update query cache with subscription data

--- graphql-chat/client/src/Chat.js
+++ graphql-chat/client/src/Chat.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useQuery, useMutation, useSubscription } from '@apollo/react-hooks';
 import {
   messagesQuery,
@@ -9,13 +9,16 @@ import MessageInput from './MessageInput';
 import MessageList from './MessageList';
 
 const Chat = ({ user }) => {
-  const [messages, setMessages] = useState([]);
-  useQuery(messagesQuery, {
-    onCompleted: (data) => setMessages(data.messages),
-  });
+  const { data } = useQuery(messagesQuery);
+  const messages = data?.messages ?? [];
+
   useSubscription(messageAddedSubscription, {
-    onSubscriptionData: ({ subscriptionData }) => {
-      setMessages(messages.concat(subscriptionData.data.messageAdded));
+    onSubscriptionData: ({ client, subscriptionData }) => {
+      client.writeData({
+        data: {
+          messages: messages.concat(subscriptionData.data.messageAdded),
+        },
+      });
     },
   });
   const [addMessage] = useMutation(addMessageMutation);
