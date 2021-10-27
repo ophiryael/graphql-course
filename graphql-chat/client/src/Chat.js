d68f5c70ac923da294ade4bd7db49c70d7a7fbaf
Ophir Yael <ofir.yael@gmail.com>
2021-10-27 21:17:40 +0300

    Set up apollo client and fetch using useQuery

--- graphql-chat/client/src/Chat.js
+++ graphql-chat/client/src/Chat.js
@@ -1,43 +1,26 @@
-import React, { Component } from 'react';
-import { addMessage, getMessages, onMessageAdded } from './graphql/queries';
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { messagesQuery } from './graphql/queries';
 import MessageInput from './MessageInput';
 import MessageList from './MessageList';
 
-class Chat extends Component {
-  state = { messages: [] };
-  subscription = null;
+const Chat = ({ user }) => {
+  const { data } = useQuery(messagesQuery);
+  const messages = data?.messages ?? [];
 
-  async componentDidMount() {
-    const messages = await getMessages();
-    this.setState({ messages });
-    this.subscription = onMessageAdded((message) => {
-      this.setState({ messages: this.state.messages.concat(message) });
-    });
-  }
+  const handleSend = (text) => {
+    // TODO
+  };
 
-  componentWillUnmount() {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
-  }
-
-  async handleSend(text) {
-    await addMessage(text);
-  }
-
-  render() {
-    const { user } = this.props;
-    const { messages } = this.state;
-    return (
-      <section className="section">
-        <div className="container">
-          <h1 className="title">Chatting as {user}</h1>
-          <MessageList user={user} messages={messages} />
-          <MessageInput onSend={this.handleSend.bind(this)} />
-        </div>
-      </section>
-    );
-  }
-}
+  return (
+    <section className="section">
+      <div className="container">
+        <h1 className="title">Chatting as {user}</h1>
+        <MessageList user={user} messages={messages} />
+        <MessageInput onSend={handleSend} />
+      </div>
+    </section>
+  );
+};
 
 export default Chat;
